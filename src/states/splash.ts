995eb223d9f0dcb19e18f8c27e018e38f46d5007
Johannes Huber <huber.johannes@online.de>
2017-11-18 17:16:18 +0100

    Nicer Splashscreen

--- src/states/splash.ts
+++ src/states/splash.ts
@@ -24,6 +24,11 @@ export class SplashState extends Phaser.State {
     let background = this.add.tileSprite(0, 0, 1024, 576, 'background');
     background.fixedToCamera = true;
     this.banner = this.add.text(350, this.game.height / 2, "Press UP to play!", {});
+    this.banner.font = 'Nunito';
+    this.banner.fontSize = 40;
+    this.banner.fill = '#ffffff';
+    this.banner.align = "center";45
+    this.banner.anchor.setTo(0, 0);
     this.cursors = this.game.input.keyboard.createCursorKeys();
     // this.game.state.start('Game');
 
