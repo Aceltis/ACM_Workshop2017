a2e893eb0e01779b52ade2ed17dc47c10df1ec83
Johannes Huber <huber.johannes@online.de>
2017-11-18 15:46:11 +0100

    Add timer

--- src/states/game.ts
+++ src/states/game.ts
@@ -4,7 +4,8 @@ import {Level} from "../entities/level";
 
 export class GameState extends Phaser.State {
   character: Character;
-  level : Level
+  level : Level;
+  timer: Phaser.Text;
 
   init () {}
   preload () {}
@@ -13,6 +14,7 @@ export class GameState extends Phaser.State {
     let background = this.add.tileSprite(0, 0, 1024, 576, 'background');
     background.fixedToCamera = true;
     let banner = this.add.text(350, 20, "You must survive!", {});
+    this.timer = this.add.text(70, 20, "0", {});
     banner.font = 'Nunito';
     banner.fontSize = 40;
     banner.fill = '#ffffff';
@@ -28,6 +30,7 @@ export class GameState extends Phaser.State {
   render () {}
 
   update() {
+    this.timer.setText(this.time.totalElapsedSeconds().toFixed(2).toString());
     this.level.update();
   }
 
