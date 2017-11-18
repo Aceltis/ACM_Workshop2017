bc45b717e47d1b126191f5dbdac323d6134a7f0d
Johannes Huber <huber.johannes@online.de>
2017-11-18 11:24:52 +0100

    Add basic character movement

--- src/entities/character.ts
+++ src/entities/character.ts
@@ -1,13 +1,27 @@
 export class Character {
+
+    private sprite: Phaser.Sprite;
+    private cursors: Phaser.CursorKeys;
+
     constructor (
+        public game: Phaser.Game,
         public x: number,
         public y: number,
         public assetUrl: string
     ) {
-        // Load sprite
+        this.sprite = this.game.add.sprite(0, 0, "mushroom");
+        this.sprite.anchor.setTo(0.1, 0.1);
+        this.cursors = this.game.input.keyboard.createCursorKeys();
     }
 
     update () {
-        // Happens every frame
+        if (this.cursors.left.isDown)
+        {
+            this.sprite.x -= 1;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.sprite.x += 1;
+        }
     }
 }
