ba25311d15dc28efdff8d3e962cec9b035d0958d
Micael Loberg <micaelloberg@gmail.com>
2017-11-18 11:43:48 +0100

    add platform to the world

--- src/states/game.ts
+++ src/states/game.ts
@@ -1,4 +1,5 @@
 import {Character} from "../entities/character";
+import {Platform} from "../entities/platform";
 
 export class GameState extends Phaser.State {
   character: Character;
@@ -13,6 +14,7 @@ export class GameState extends Phaser.State {
     banner.fill = '#77BFA3';
     banner.anchor.setTo(0, 0);
 
+    let testPlatform = new Platform(this, 100, 100, 100, 50, "ground");
     let mushroom = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "mushroom");
     mushroom.anchor.setTo(0.5, 0.5);
   }
