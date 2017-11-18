177cb9a05c65b3754e12646caca2044c28e1e0fa
Micael Loberg <micaelloberg@gmail.com>
2017-11-18 11:35:52 +0100

    initial platform implementation

--- src/entities/platform.ts
+++ src/entities/platform.ts
@@ -1,10 +1,17 @@
+import {GameState} from "../states/game";
+
 export class Platform {
+    sprite: Phaser.TileSprite;
     constructor (
+        public game: GameState,
         public x: number,
         public y: number,
-        public assetUrl: string
+        public w: number,
+        public h: number,
+        public assetID: string
     ) {
         // Load sprite
+        this.sprite = this.game.add.tileSprite(x, y, w, h, assetID);
     }
 
     update () {
