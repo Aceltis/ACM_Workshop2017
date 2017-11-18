ab6bc66e00c69a775abad02d62fc1d9c6b6e62d8
Tommy Vagbratt <tommy@vagbratt.se>
2017-11-18 14:32:03 +0100

    Very basic random platform spawning

--- src/entities/level.ts
+++ src/entities/level.ts
@@ -6,6 +6,7 @@ export class Level {
 
   groundPhysicsGroup : Phaser.Group;
   platforms : Platform[] = [];
+  time : number = 0;
 
   constructor (
     public game : Phaser.Game,
@@ -20,11 +21,27 @@ export class Level {
     this.platforms.push(platform)
   }
 
+  spawnPlatforms() {
+    this.time += this.game.time.elapsed;
+
+
+    if( this.time > 3000 ) {
+      this.createPlatform(this.game.width, 300 + Math.random() * 200);
+      this.time = 0;
+      console.log("spawn platform");
+    }
+
+
+  }
+
   update () {
     // Happens every frame
     this.game.physics.arcade.collide(this.character.sprite, this.groundPhysicsGroup);
     this.game.physics.arcade.collide(this.groundPhysicsGroup, this.character.sprite);
     this.character.update();
     this.platforms.forEach(platform => platform.update());
+
+    this.spawnPlatforms();
+
   }
 }
