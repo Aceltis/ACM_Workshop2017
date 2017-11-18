29fb9df4543f8636dace4244457d6ff2b85a2065
Johannes Huber <huber.johannes@online.de>
2017-11-18 13:23:06 +0100

    Separate acceleration and deacceleration

--- src/entities/character.ts
+++ src/entities/character.ts
@@ -3,9 +3,13 @@ export class Character {
     private sprite: Phaser.Sprite;
     private cursors: Phaser.CursorKeys;
 
-    private maxSpeed: number = 400;
     private inAir: boolean = false;
-    private jumpTimer: number = 0;
+    private doubleJumped: boolean = false;
+
+    private accSpeed: number = 20;
+    private deAccSpeed: number = 60;
+    private maxSpeed: number = 600;
+    private jumpHeight: number = 600;
 
     constructor (
         public game: Phaser.Game,
@@ -33,21 +37,27 @@ export class Character {
             this.inAir = false;
         }
 
+        let speed = this.accSpeed;
         if (this.cursors.left.isDown){
+            if(this.sprite.body.velocity.x > 0) speed = this.deAccSpeed;
             if(this.sprite.body.velocity.x > (this.maxSpeed * -1)){
-                this.sprite.body.velocity.x -= 20;
+                this.sprite.body.velocity.x -= speed;
             }
         }
         else if (this.cursors.right.isDown){
+            if(this.sprite.body.velocity.x < 0) speed = this.deAccSpeed;
             if(this.sprite.body.velocity.x < this.maxSpeed){
-                this.sprite.body.velocity.x += 20;
+                this.sprite.body.velocity.x += speed;
             }
         }
 
         if (this.cursors.up.isDown){
-            if(!this.inAir){
+            if(!this.inAir || !this.doubleJumped){
+                if(this.inAir){
+                    this.doubleJumped = true;
+                }
                 this.inAir = true;
-                this.sprite.body.velocity.y = -400;
+                this.sprite.body.velocity.y = -1 * this.jumpHeight;
             }
         }
     }
