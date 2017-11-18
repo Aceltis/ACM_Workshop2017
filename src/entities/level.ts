e0d5797f1c8ea400cfeab812b5ed93890c7e1f1e
Micael Loberg <micaelloberg@gmail.com>
2017-11-18 13:17:41 +0100

    pass reference of character to level

--- src/entities/level.ts
+++ src/entities/level.ts
@@ -1,21 +1,26 @@
 import {Platform} from "../entities/platform";
+import {Character} from "../entities/character";
+
 
 export class Level {
 
   groundPhysicsGroup : Phaser.Group
 
   constructor (
-    public game : Phaser.Game
+    public game : Phaser.Game,
+    public character : Character
   ) {
     // Load sprite
     this.groundPhysicsGroup = this.game.add.physicsGroup();
   }
 
   createPlatform() {
-    let platform = new Platform(this.game, this.groundPhysicsGroup, 100, 100, 100, 50, "ground");
+    let platform = new Platform(this.game, this.groundPhysicsGroup, 100, 500, 100, 50, "ground");
   }
 
   update () {
     // Happens every frame
+    this.game.physics.arcade.collide(this.character.sprite, this.groundPhysicsGroup);
+    this.character.update();
   }
 }
